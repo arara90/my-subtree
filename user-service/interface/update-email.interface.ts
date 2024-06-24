28d4f78f66d3798400c4e2cfd680f6a8cfc65107
arara90 <ararajo@gmail.com>
2024-06-24 12:49:39 +0900

    4. 불필요한 email 인터페이스 삭제 & phone 인터페에스에 user 정보 추가

--- user-service/interface/update-email.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface UpdateEmail {
-  email: string
-}
--- user-service/interface/update-phone.interface.ts
+++ user-service/interface/update-phone.interface.ts
@@ -2,4 +2,6 @@ export interface UpdatePhone {
   country_code: string
   country_calling_code: number
   phone: string
+  user_id: number
+  username: string
 }
